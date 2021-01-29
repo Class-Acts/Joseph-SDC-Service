5f941778ae832c6f7539a820fa657cea59908b29
Paul Van Leuven <paulcharlesvl@gmail.com>
2021-01-29 14:56:33 -0600

    change database guy

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
+  user: 'someguy',
+  password: 'password',
   database: 'rgidata'
 })
 
